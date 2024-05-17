bb8c87f4f07752c5adf2c53bbb7ec12195bcf4c7
Acgpro1Aymir1 <149924049+Acgpro1Aymir1@users.noreply.github.com>
2024-05-17 13:08:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" #online/dnd/idle
+status = "dnd" #online/dnd/idle
 
-custom_status = "youtube.com/@SealedSaucer" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
+custom_status = "🔒 DOH/KOO. I want all the smoke like a vape pen. 2 Shots up in the Glock and I’m hitting all head taps" #If you don't need a custom status on your profile, just put "" instead of "youtube.com/@SealedSaucer"
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
